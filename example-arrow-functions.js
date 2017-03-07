f8e0b257a1854e306d2357f88cb5d044b4094149
Nik Reynolds <nik.reynolds@piksel.com>
2017-03-07 13:32:01 +0000

    Initial commit

---
+++ example-arrow-functions.js
@@ -0,0 +1,47 @@
+var names = ['Nik', 'Paul', 'Walker'];
+//
+// // names.forEach(function (name) {
+// //   console.log('forEach', name);
+// // });
+// //
+// // names.forEach((name) => {
+// //   console.log('arrowFunc', name);
+// // });
+// //
+// // names.forEach((name) => console.log(name));
+//
+// var returnMe = (name) => name + '!';
+//
+// console.log(returnMe('Nik'));
+//
+// var person = {
+//   name: 'Nik',
+//   greet: function () {
+//     names.forEach((name) => {
+//       console.log(this.name + ' says hi to ' + name);
+//     })
+//   }
+// }
+//
+// person.greet();
+
+
+// Challenge Area
+function add(a,b) {
+  return a + b;
+}
+
+// addStatement
+var addStatement = (a, b) => {
+  return a + b;
+}
+
+// addExpression
+var addExpression = (a, b) => a + b;
+
+
+console.log(add(1,3));
+console.log(add(9,0));
+
+console.log(addStatement(4, 7));
+console.log(addExpression(3, -2));
