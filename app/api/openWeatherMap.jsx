ceade3bb2eb3042d6c4014a601ee1a678638eff7
Nik Reynolds <nik.reynolds@piksel.com>
2017-03-20 14:56:32 +0000

    Added the ErrorModal.

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -3,23 +3,18 @@ var axios = require('axios');
 const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=74d58025e46eccb619fb37fae8e1af18&units=metric';
 
 module.exports = {
-  getTemp: function(location) {
+  getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function (res) {
-      debugger;
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
       }
-    }, function (res) {
-      if (res.data) {
-        throw new Error('Unable to fetch weather for the location.');
-      } else {
-        throw new Error(res);
-      }
+    }, function (err) {
+      throw new Error(err.response.data.message);
     });
   }
 }
